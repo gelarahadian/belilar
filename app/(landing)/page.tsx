0e24455f973a242463a418301936386e76a1f886
gelarahadian <gelarahadian38@gmail.com>
2024-08-11 21:35:42 +0700

    add pagination to the langding page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import ProductCard from "./components/ProductCard";
+import Pagination from "./components/Pagination";
+import { Product } from "@/context/product";
 
-const fetchProducts = async () => {
+const fetchProducts = async (
+  page: string
+): Promise<{
+  currentPage: number;
+  totalProducts: number;
+  totalPages: number;
+  products: Product[];
+}> => {
   try {
-    const res = await fetch(`${process.env.API}/product`);
+    const res = await fetch(`${process.env.API}/product?page=${page}`, {
+      cache: "no-store",
+    });
     if (!res.ok) {
       console.log("Fetching products Error: " + res);
     }
@@ -17,8 +28,13 @@ const fetchProducts = async () => {
   }
 };
 
-const page = async () => {
-  const { products } = await fetchProducts();
+const page = async ({ searchParams }: { searchParams: { page: string } }) => {
+  console.log(searchParams);
+  const { currentPage, totalProducts, totalPages, products } =
+    await fetchProducts(searchParams.page);
+
+  console.log("totalProducts====>", totalProducts);
+  console.log("totalPages====>", totalPages);
   return (
     <main className="max-w-6xl mx-auto w-full p-3">
       <h1 className="text-2xl font-bold text-center mb-3">Product Unggul</h1>
@@ -28,6 +44,7 @@ const page = async () => {
             <ProductCard key={product.id} product={product} />
           ))}
       </ul>
+      <Pagination currentPage={currentPage} totalPages={totalPages} />
     </main>
   );
 };
