75a8811985d46f6298ad62ec8b1c6e8d457569be
gelarahadian <gelarahadian38@gmail.com>
2024-08-12 11:16:59 +0700

    fix: make Header component client

--- app/(global)/dashboard/components/Header.tsx
+++ app/(global)/dashboard/components/Header.tsx
@@ -1,20 +1,18 @@
-import { auth } from "@/auth";
+"use client";
 import Image from "next/image";
 import React from "react";
 
-const Header = async () => {
-  const session = await auth();
-  console.log(session?.user);
+const Header = () => {
   return (
     <header className="flex justify-end items-center px-4 h-14 border-b-2 ">
       <div className="flex items-center space-x-3">
-        <p className="text-body">{session?.user.name}</p>
+        {/* <p className="text-body">{session?.user.name}</p>
         <Image
           src={session?.user.image || "/user.png"}
           alt={session?.user?.name || ""}
           height={36}
           width={36}
-        />
+        /> */}
       </div>
     </header>
   );
