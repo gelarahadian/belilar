5a78284a7a0f309a844d02f6e0d13674ba9cf624
gelarahadian <gelarahadian38@gmail.com>
2025-10-02 15:58:32 +0700

    footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -5,12 +5,19 @@ import Description from './Description'
 
 const Footer = () => {
   return (
-    <div className='max-w-7xl mx-auto text-gray-500'>
-      <Description/>
-      <AppFooter/>
-      <CopyrightAndLanguage/>
-    </div>
-  )
+    <>
+      <div className="bg-white py-4 border-t border-t-secondary mt-4 ">
+        <div className="max-w-7xl mx-auto text-gray-500">
+          <Description />
+        </div>
+      </div>
+
+      <div className="max-w-7xl mx-auto text-gray-500">
+        <AppFooter />
+        <CopyrightAndLanguage />
+      </div>
+    </>
+  );
 }
 
 export default Footer
